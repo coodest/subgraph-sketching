4cf4baadfac2efb584d8eb11c9bb97860b3d77e7
Heptagram <350526878@qq.com>
2025-11-28 14:43:26 +0800

    20251128 finalized.

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
-# python -m src.runners.run --dataset_name wikidata --model BUDDY
-# python -m src.runners.run --dataset_name twitter --model BUDDY
-# python -m src.runners.run --dataset_name ppi --model BUDDY
-# python -m src.runners.run --dataset_name dblp --model BUDDY
-# python -m src.runners.run --dataset_name blogcatalog --model BUDDY
 
-python -m src.runners.run --dataset_name wikidata1k_multiclass --model BUDDY --multiclass
-# python -m src.runners.run --dataset_name wikidata5k_multiclass --model BUDDY --multiclass
-# python -m src.runners.run --dataset_name wikidata10k_multiclass --model BUDDY --multiclass
+prefix="docker run --rm -it -v ./:/home/worker/work --gpus all meetingdocker/ml:ss python -m src.runners.run"
+# prefix="python -m src.runners.run"
+
+# $prefix --dataset_name wikidata --model BUDDY
+# $prefix --dataset_name twitter --model BUDDY
+# $prefix --dataset_name ppi --model BUDDY
+# $prefix --dataset_name dblp --model BUDDY
+# $prefix --dataset_name blogcatalog --model BUDDY
+
+$prefix --dataset_name wikidata1k_multiclass --model BUDDY --multiclass
+# $prefix --dataset_name wikidata5k_multiclass --model BUDDY --multiclass
+# $prefix --dataset_name wikidata10k_multiclass --model BUDDY --multiclass
